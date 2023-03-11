bdef48b88deff748ae9537906cc063099cff8eec
JD-Flores <90649355+JD-Flores@users.noreply.github.com>
2023-03-11 18:54:34 -0400

    Diseño barra buscador

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Routes,Route,BrowserRouter} from "react-router-dom"
-import {HOME_URL, LOGIN_URL, REGISTER_URL, PROFILE_CLIENT, MOVIES_URL } from './constantes/urls'
+import {HOME_URL, LOGIN_URL, REGISTER_URL, PROFILE_CLIENT, MOVIES_URL, SEARCH_URL } from './constantes/urls'
 import { Layout } from './Componentes/Layout/Layout'
 import {HomePage} from './Paginas/Home/HomePage'
 import {RegisterPage} from './Paginas/register/RegisterPage'
 import { LoginPage } from './Paginas/login/LoginPage'
+import { SearchPage } from './Paginas/Search/SearchPage'
 import ErrorPage from './Paginas/error/ErrorPage'
 
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={REGISTER_URL} element={<RegisterPage/>}/>
         <Route path={LOGIN_URL} element={<LoginPage/>}/>
         <Route path={MOVIES_URL} element={<HomePage/>}/>
+        <Route path={SEARCH_URL} element={<SearchPage/>}/>
         <Route path="/*" element={<ErrorPage/>}/>
         </Route>
       </Routes>
