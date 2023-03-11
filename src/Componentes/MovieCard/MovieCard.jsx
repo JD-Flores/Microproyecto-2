5fd88dd6ad9cc4780220f9d60301e82ee5eefdee
martinrojasa <martinrojasa@outlook.com>
2023-03-11 17:34:56 -0400

    movie card

--- src/Componentes/MovieCard/MovieCard.jsx
+++ src/Componentes/MovieCard/MovieCard.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 export function MovieCard () {
     return (
-        <div className="bg-green-700 h-80 w-11/12 rounded-3xl shadow-lg p-4 text-xl flex">
-            <img src="https://image.tmdb.org/t/p/w92/bvYjhsbxOBwpm8xLE5BhdA3a8CZ.jpg" alt="poster" />
-            <div>
-                <h1 className="text-2xl bg-gray-500 font-bold">Titulo</h1>
-                <h1 className="bg-gray-500">Popularidad</h1>
-                <h1 className="bg-gray-500">Idioma</h1>
+        <div className="h-auto w-11/12 rounded-3xl shadow-lg p-4 text-xl flex border-2 border-gray-100 max-w-sm items-center">
+            <div className="h-auto w-1/2">
+                <img src="https://image.tmdb.org/t/p/w92/bvYjhsbxOBwpm8xLE5BhdA3a8CZ.jpg" alt="poster" className="w-full"/>
+            </div>
+            
+            <div className="p-4">
+                <h1 className="text-2xl font-bold">Titulo</h1>
+                <h1 className=""><span className="font-semibold">Popularidad:</span>&nbsp;180.000</h1>
+                <h1 className=""><span className="font-semibold">Idioma:</span>&nbsp;en</h1>
             </div>
             
         </div>
--- src/Paginas/Home/HomePage.jsx
+++ src/Paginas/Home/HomePage.jsx
@@ -6,8 +6,13 @@ import { MovieCard } from '../../Componentes/MovieCard/MovieCard'
 export function HomePage() {
 
   return (
-    <div className="flex flex-col items-center pt-4">
-      <MovieCard/>
+    <div>
+      <div className="flex flex-wrap items-center justify-between m-4 gap-y-3 mt-12">
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+      </div>
     </div>
   )
 }
