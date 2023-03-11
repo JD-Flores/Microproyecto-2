5f03a7820f2a90448984c5b699cf2431c53baba0
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-03-11 17:36:21 -0400

    pepe

--- src/Paginas/Home/HomePage.jsx
+++ src/Paginas/Home/HomePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MovieCard } from '../../Componentes/MovieCard/MovieCard'
+import { fetchMovies } from '../../utils/movies-api'
 
 
 
 export function HomePage() {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setLoading] = useState(false);
+
+  const getMovies = async () => {
+    setLoading(true)
+    const {data} = await fetchMovies()
+    setMovies(data.results)
+
+    console.log(data) 
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+
 
   return (
     <div className="flex flex-col items-center pt-4">
