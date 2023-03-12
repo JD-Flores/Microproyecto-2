b7156c00b293e5770219c5d3c87c69473f6ec3b7
martinrojasa <martinrojasa@outlook.com>
2023-03-12 16:54:30 -0400

    movie details and cards ready

--- src/Componentes/MovieCard/MovieCard.jsx
+++ src/Componentes/MovieCard/MovieCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export function MovieCard (props) {
     const { title, poster_path, original_language, popularity } = props.movie;
-    const{ languages } = props.languages;
 
     return (
         <div className="h-auto w-11/12 rounded-3xl shadow-lg p-4 text-xl flex border-2 border-gray-100 max-w-sm items-center">
@@ -14,8 +13,8 @@ export function MovieCard (props) {
                 <h1 className="text-2xl font-bold">{title}</h1>
                 <h1 className=""><span className="font-semibold">Popularidad:</span>&nbsp;{popularity}</h1>
                 <h1 className=""><span className="font-semibold">Idioma:</span>&nbsp;
-                {languages && languages
-                .filter(language => language.iso_639_1 === movie.original_language)
+                {props.languages && props.languages
+                .filter(language => language.iso_639_1 === original_language)
                 .map(language => language.english_name)
                 }
                 </h1>
