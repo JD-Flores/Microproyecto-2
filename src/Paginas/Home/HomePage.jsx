022db151214d9fdc2e5f4ee08fad3b53b0ebc73d
martinrojasa <martinrojasa@outlook.com>
2023-03-11 21:23:49 -0400

    minor fix

--- src/Paginas/Home/HomePage.jsx
+++ src/Paginas/Home/HomePage.jsx
@@ -38,8 +38,8 @@ export function HomePage() {
   return (
     <div>
       <h1 className="text-4xl font-bold ml-8 mt-4">Peliculas</h1>
-      <button className={`w-1/5 px-5 font-comfortaa text-blue-900 rounded-lg drop-shadow-lg hover:text-slate-300 transition duration-400 ease-out px-5 m-4 ${estrenos === 0 ? 'bg-sky-700 text-gray-100' : 'border-solid border-slate-600 border-2'}`}>Peliculas Comunes</button>
-      <button className={`w-1/5 px-5 font-comfortaa text-blue-900 rounded-lg drop-shadow-lg hover:text-slate-300 transition duration-400 ease-out px-5 m-4 ${estrenos === 1 ? 'bg-sky-700 text-gray-100' : 'border-solid border-slate-600 border-2'}`}>Proximos estrenos</button>
+      <button className={`w-1/5 px-5 font-comfortaa text-blue-900 rounded-lg drop-shadow-lg hover:text-slate-300 transition duration-400 ease-out m-4 ${estrenos === 0 ? 'bg-sky-700 text-gray-100' : 'border-solid border-slate-600 border-2'}`}>Peliculas Comunes</button>
+      <button className={`w-1/5 px-5 font-comfortaa text-blue-900 rounded-lg drop-shadow-lg hover:text-slate-300 transition duration-400 ease-out m-4 ${estrenos === 1 ? 'bg-sky-700 text-gray-100' : 'border-solid border-slate-600 border-2'}`}>Proximos estrenos</button>
       <div className="flex flex-wrap items-center justify-between m-4 gap-y-4 mt-8">
         {movies.map((movie, idx) => (
         <MovieCard key={idx} movie={movie} />
