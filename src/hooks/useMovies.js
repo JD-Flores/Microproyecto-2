47b95b0d15c585c7f889ae442385c31e490b0960
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-03-12 16:20:11 -0400

    lester3.0

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -5,21 +5,26 @@ import { fetchUpcomingMovies } from "../utils/movies-api";
 
 export function useMovies() {
     const [movies, setMovies] = useState([])
+    const [total_pages, setPages] = useState()
     const [isLoading, setLoading] = useState(false);
 
-    const getUpcomingMovies = async () => {
+    const getUpcomingMovies = async (page) => {
         setLoading(true)
-        const {data} = await fetchUpcomingMovies()
+        const {data} = await fetchUpcomingMovies(page)
         
+        setPages(data.total_pages)
         setMovies(data.results)
+        setLoading(false)
     }
     
-    const getMovies = async () => {
+    const getMovies = async (page) => {
         setLoading(true)
-        const {data} = await fetchMovies()
+        const {data} = await fetchMovies(page)
         
+        setPages(data.total_pages)
         setMovies(data.results)
+        setLoading(false)
     }
-    return {movies, isLoading, getMovies, getUpcomingMovies}
+    return {movies, total_pages, isLoading, getMovies, getUpcomingMovies}
 }
 
--- src/utils/movies-api.js
+++ src/utils/movies-api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export async function fetchMovies() {
-    return axios.get("https://api.themoviedb.org/3/discover/movie?api_key=69e576331d7baf30beab2a4c59945ff5")
+export async function fetchMovies(page) {
+    return axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=69e576331d7baf30beab2a4c59945ff5&page=${page}`)
 }
 
-export async function fetchUpcomingMovies() {
-    return axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=69e576331d7baf30beab2a4c59945ff5")
+export async function fetchUpcomingMovies(page) {
+    return axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=69e576331d7baf30beab2a4c59945ff5&page=${page}`)
 }
 
 
