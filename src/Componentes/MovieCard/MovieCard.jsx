b9e906bc7ebd181f530dd00ed363ab49b1c5f5b2
martinrojasa <martinrojasa@outlook.com>
2023-03-12 17:24:05 -0400

    movie card and movie details fixed

--- src/Componentes/MovieCard/MovieCard.jsx
+++ src/Componentes/MovieCard/MovieCard.jsx
@@ -4,15 +4,13 @@ export function MovieCard (props) {
     const { title, poster_path, original_language, popularity } = props.movie;
 
     return (
-        <div className="h-auto w-11/12 rounded-3xl shadow-lg p-4 text-xl flex border-2 border-gray-100 max-w-sm items-center">
-            <div className="h-auto w-1/2">
-                <img src={"https://image.tmdb.org/t/p/w92/" + poster_path} alt="poster" className="w-full"/>
-            </div>
+        <div className="rounded-3xl shadow-lg p-4 text-xl flex border-2 border-gray-100 items-center sm:h-48 sm:w-96 h-44 w-80">
+                <img src={"https://image.tmdb.org/t/p/w92/" + poster_path} alt="poster" className="h-auto w-24"/>
 
             <div className="p-4">
-                <h1 className="text-2xl font-bold">{title}</h1>
-                <h1 className=""><span className="font-semibold">Popularidad:</span>&nbsp;{popularity}</h1>
-                <h1 className=""><span className="font-semibold">Idioma:</span>&nbsp;
+                <h1 className="sm:text-2xl font-bold text-xl pb-2">{title}</h1>
+                <h1 className="text-sm sm:text-base"><span className="font-semibold">Popularidad:</span>&nbsp;{popularity}</h1>
+                <h1 className="text-sm sm:text-base"><span className="font-semibold">Idioma:</span>&nbsp;
                 {props.languages && props.languages
                 .filter(language => language.iso_639_1 === original_language)
                 .map(language => language.english_name)
